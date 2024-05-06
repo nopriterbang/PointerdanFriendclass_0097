292376535a599f5f988a3a4b80a6fddc604a5071
nopriterbang <novalpriya@gmail.com>
2024-05-06 15:09:30 +0700

    membuat class siswa dengan mengunakan access modifier

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -7,4 +7,16 @@ class orang
 private:
     string nama;
 
-}
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
